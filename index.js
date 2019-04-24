37de1994bc494dc939f585c3b2c19973088f8177
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-24 02:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+
+function nowServing(line) {
+  return line.length == 0 ? "There is nobody waiting to be served!" : line.pop();
+}
